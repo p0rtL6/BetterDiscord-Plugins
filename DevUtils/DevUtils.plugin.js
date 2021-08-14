34b8186ad454862c89850dba4af590b3374e5745
p0rtL6 <80869244+p0rtL6@users.noreply.github.com>
2021-08-14 16:44:39 -0400

    Add Dropdown

--- DevUtils/DevUtils.plugin.js
+++ DevUtils/DevUtils.plugin.js
@@ -2,10 +2,9 @@
  * @name DevUtils
  * @author p0rtL
  * @description Basic Developer tools for BD
- * @version 1.0 Alpha
+ * @version 1.1 Alpha
  */
 
-const zLibModules = Object.keys(ZLibrary.DiscordModules)
 
 const createUpdateWrapper = (Component, valueProp = "value", changeProp = "onChange") => props => {
     const [value, setValue] = BdApi.React.useState(props[valueProp]);
@@ -19,18 +18,13 @@ const createUpdateWrapper = (Component, valueProp = "value", changeProp = "onCha
     });
 }
 
-const TextInput = createUpdateWrapper(BdApi.findModuleByDisplayName("TextInput"));
-const Button = BdApi.findModuleByProps("Button").Button;
-
-function updateCss() {
-    if (document.querySelector("bd-styles #DevUtils")) {
-        BdApi.clearCSS("DevUtils");
-        BdApi.injectCSS("DevUtils", `@import url(https://p0rtl6.github.io/BetterDiscord-Plugins/DevUtils/styles.css)`);
-    }
-    else { BdApi.injectCSS("DevUtils", `@import url(https://p0rtl6.github.io/BetterDiscord-Plugins/DevUtils/styles.css)`); }
-}
-
 const {React} = BdApi;
+const Button = BdApi.findModuleByProps("Button").Button;
+const zLibModules = Object.keys(ZLibrary.DiscordModules);
+const SettingsView = BdApi.findModuleByDisplayName("SettingsView");
+const menuTimes = ["Select Module", "button", "TextInput", "Custom Input"];
+const TextInput = createUpdateWrapper(BdApi.findModuleByDisplayName("TextInput"));
+const Dropdown = createUpdateWrapper(BdApi.findModuleByProps("SingleSelect").SingleSelect);
 
 class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -56,11 +50,19 @@ class ErrorBoundary extends React.Component {
     }
 }
 
+function updateCss() {
+    if (document.querySelector("bd-styles #DevUtils")) {
+        BdApi.clearCSS("DevUtils");
+        BdApi.injectCSS("DevUtils", ``);
+    }
+    else { BdApi.injectCSS("DevUtils", ``); }
+    //@import url(https://p0rtl6.github.io/BetterDiscord-Plugins/DevUtils/styles.css)
+}
+
 module.exports = class DevUtils {
     load() { }
     start() {
         updateCss()
-        const SettingsView = BdApi.findModuleByDisplayName("SettingsView");
 
         const Store = this._userSettingsStore = BdApi.findModule(m=>m.toString().indexOf("useStore") > -1);
         
@@ -70,6 +72,53 @@ module.exports = class DevUtils {
             return React.createElement(ErrorBoundary, null, element);
         }; StoreWrapper.forceUpdate = () => {};
 
+        function searchModules() {
+            let res = {}
+            let query = BdApi.getData("DevUtils", "ModuleSearch")
+            if (zLibModules.includes(query)) {
+                res.title = "Found Webpack Module"
+                res.module = ZLibrary.DiscordModules[query]
+                res.access = ` ZLibrary.DiscordModules.${query}`
+            } else {
+                if (BdApi.findModuleByDisplayName(query)) {
+                    res.title = "Found Module"
+                    res.module = BdApi.findModuleByDisplayName(query)
+                    res.access = `BdApi.findModuleByDisplayName('${query}')`
+                }
+                else if (BdApi.findModuleByProps(query)) {
+                    res.title = "Found Module"
+                    res.module = BdApi.findModuleByProps(query)
+                    res.access = `BdApi.findModuleByProps('${query}')`
+                } 
+                else if (BdApi.findModule((m) => m?.default?.displayName === query)) {
+                    res.title = "Found Module"
+                    res.module = BdApi.findModule((m) => m?.default?.displayName === query)
+                    res.access = `BdApi.findModule((m) => m?.default?.displayName === '${query}')`
+                }
+                else {
+                    res.title = "No Module Found"
+                    res.module = "No Module"
+                    res.access = `No Module`
+                }
+            }
+            
+            BdApi.showConfirmationModal(res.title, React.createElement(popupContent, {
+                res: res
+            }), {
+                confirmText: "close",
+            });
+        }
+
+        function buildMenu(menuTimes) {
+            var menuOptions = menuTimes.map((item) => {
+                return {
+                    label: item,
+                    value: item
+                };
+            })
+            return menuOptions
+        }
+        
         class popupContent extends React.Component {
 
             constructor(props) {
@@ -117,89 +166,79 @@ module.exports = class DevUtils {
             }   
         }
 
-        function searchModules() {
-            let res = {}
-            let query = BdApi.getData("DevUtils", "ModuleSearch")
-            if (zLibModules.includes(query)) {
-                res.title = "Found Webpack Module"
-                res.module = ZLibrary.DiscordModules[query]
-                res.access = ` ZLibrary.DiscordModules.${query}`
-            } else {
-                if (BdApi.findModuleByDisplayName(query)) {
-                    res.title = "Found Module"
-                    res.module = BdApi.findModuleByDisplayName(query)
-                    res.access = `BdApi.findModuleByDisplayName('${query}')`
-                }
-                else if (BdApi.findModuleByProps(query)) {
-                    res.title = "Found Module"
-                    res.module = BdApi.findModuleByProps(query)
-                    res.access = `BdApi.findModuleByProps('${query}')`
-                } 
-                else if (BdApi.findModule((m) => m?.default?.displayName === query)) {
-                    res.title = "Found Module"
-                    res.module = BdApi.findModule((m) => m?.default?.displayName === query)
-                    res.access = `BdApi.findModule((m) => m?.default?.displayName === '${query}')`
-                }
-                else {
-                    res.title = "No Module Found"
-                    res.module = "No Module"
-                    res.access = `No Module`
-                }
-            }
-            
-            BdApi.showConfirmationModal(res.title, React.createElement(popupContent, {
-                res: res
-            }), {
-                confirmText: "close",
-            });
-        }
+        class createUtilsPanel extends React.Component {
 
-        function createUtilsPanel() {
-            return React.createElement(
-                "div",
-                {
-                id: "DevUtilsWrapper"
-                },
-                React.createElement("h1", {
-                            style: {
-                                color: "#ddd",
-                                marginBottom: 10,
-                                fontSize: 50,
-                                fontWeight: "bold"
-                            }
-                        }, "Developer Utilities"),
-                React.createElement("hr", {
-                    style: {
-                        "border": "0",
-                        "border-top": "3px solid #ddd",
-                        marginBottom: 20,
-                    }
-                }),
-                React.createElement("h1", {
-                    style: {
-                        color: "#ddd",
-                        marginBottom: 10,
-                        fontSize: 20,
-                        fontWeight: "bold"
-                    }
-                }, "Search Modules"),
-                React.createElement(TextInput, {
-                    value: BdApi.getData("DevUtils", "ModuleSearch"),
-                    style: {
-                        height: "44px"
+            constructor(props) {
+                super(props)
+                this.state = { inputVisibility: "hidden", inputWidth: "0px" }
+                BdApi.saveData("DevUtils", "ModuleDropdown", "Select Module")
+            }
+
+            render() {
+
+                return React.createElement(
+                    "div",
+                    {
+                    id: "DevUtilsWrapper"
                     },
-                    onChange: (value) => {
-                        BdApi.saveData("DevUtils", "ModuleSearch", value);
-                    }
-                }),
-                React.createElement(Button, {
-                    onClick: searchModules,
-                    style: {
-                        width: "15%",
-                    }
-                }, "Search"),
-                React.createElement(StoreWrapper, null)
-                );
+                    React.createElement("h1", {
+                                style: {
+                                    color: "#ddd",
+                                    marginBottom: 10,
+                                    fontSize: 50,
+                                    fontWeight: "bold"
+                                }
+                            }, "Developer Utilities"),
+                    React.createElement("hr", {
+                        style: {
+                            "border": "0",
+                            "border-top": "3px solid #ddd",
+                            marginBottom: 20,
+                        }
+                    }),
+                    React.createElement("h1", {
+                        style: {
+                            color: "#ddd",
+                            marginBottom: 10,
+                            fontSize: 20,
+                            fontWeight: "bold"
+                        }
+                    }, "Search Modules"),
+                    React.createElement(Dropdown, {
+                        value: BdApi.getData("DevUtils", "ModuleDropdown"),
+                        options: buildMenu(menuTimes),
+                        onChange: (value) => {
+                            BdApi.saveData("DevUtils", "ModuleDropdown", value);
+                            if (value == "Custom Input") {
+                                this.setState({ inputVisibility: "visible", inputWidth: "100%" })
+                                BdApi.saveData("DevUtils", "ModuleSearch", this.state.search);
+                            } else {
+                                this.setState({ inputVisibility: "hidden", inputWidth: "0px" })
+                                BdApi.saveData("DevUtils", "ModuleSearch", value);
+                            }
+                        }
+                    }),
+                    React.createElement(TextInput, {
+                        value: BdApi.getData("DevUtils", "ModuleSearch"),
+                        style: {
+                            height: "44px",
+                            visibility: this.state.inputVisibility,
+                            width: this.state.inputWidth
+                        },
+                        onChange: (value) => {
+                            BdApi.saveData("DevUtils", "ModuleSearch", value);
+                            this.setState({ search: value })
+                        }
+                    }),
+                    React.createElement(Button, {
+                        onClick: searchModules,
+                        style: {
+                            width: "15%",
+                        }
+                    }, "Search"),
+                    React.createElement(StoreWrapper, null)
+                    );
+            }
         }
 
         BdApi.Patcher.after("SettingsPatch", SettingsView.prototype, "componentDidMount", _this => {
@@ -208,7 +247,7 @@ module.exports = class DevUtils {
             _this.props.sections.splice(_this.props.sections.findIndex(s => s.section === "Experiments"), 0, {
                 section: "devutils",
                 label: "Dev Utils",
-                element: () => createUtilsPanel()
+                element: () => React.createElement(createUtilsPanel, null)
             });
 
             _this.forceUpdate();
